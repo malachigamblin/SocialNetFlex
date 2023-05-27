3fbf0a4b27642279acccae0bb344d8bbe6fe2176
malachigamblin <malachi.gamblin@gmail.com>
2023-05-26 23:31:52 -0400

    setup get thought by ID in thought controllers

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -1,18 +1,39 @@
-const { Thought, User } = require('../models')
+const { Thought, User } = require("../models");
 
 const thoughtController = {
-    getAllThoughts(req, res) {
-        Thought.find({})
-        .populate({
-            path: 'reactions',
-            select: '-__v',
-        })
-        .select('-__v')
-        .sort({ _id: -1 })
-        .then((dbThoughtData) => res.json(dbThoughtData))
-        .catch((e) => {
-            console.log(e)
-            res.sendStatus(400)
-        })
-    }
-}
+  getAllThoughts(req, res) {
+    Thought.find({})
+      .populate({
+        path: "reactions",
+        select: "-__v",
+      })
+      .select("-__v")
+      .sort({ _id: -1 })
+      .then((dbThoughtData) => res.json(dbThoughtData))
+      .catch((e) => {
+        console.log(e);
+        res.sendStatus(400);
+      });
+  },
+
+  getThoughtById({ params }, res) {
+    Thought.findOne({ _id: params.id })
+      .populate({
+        path: "reactions",
+        select: "-__v",
+      })
+      .select("-__v")
+      .then((dbThoughtData) => {
+        if (!dbThoughtData) {
+          return res
+            .status(404)
+            .json({ message: "There is no thought with this ID" });
+        }
+        res.json(dbThoughtData);
+      })
+      .catch((e) => {
+        console.log(e);
+        res.sendStatus(400);
+      });
+  },
+};
