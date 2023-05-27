eb357e1836e300ae9b7062af635ea1ea8e599be8
malachigamblin <malachi.gamblin@gmail.com>
2023-05-27 11:47:01 -0400

    fixed issues getting server to run

--- models/thought.js
+++ models/thought.js
@@ -62,6 +62,6 @@ ThoughtSchema.virtual("reactionCount").get(function () {
   return this.reactions.length;
 });
 
-const Thought = model("Thought, ThoughtSchema");
+const Thought = model("Thought", ThoughtSchema);
 
 module.exports = Thought;
